56fdc885543ea640f8e7ac1206c581976b0c5956
Jacobtco <jacobtco@gmail.com>
2020-09-02 16:14:08 +0800

    Added a filter anecdote store, reducer and component

---
+++ redux-anecdotes/src/components/AnecdoteFilter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnecdoteFilter = () => {
+  return (
+    <div>
+      filter <input />
+    </div>
+  );
+};
+
+export default AnecdoteFilter;
--- redux-anecdotes/src/components/App.js
+++ redux-anecdotes/src/components/App.js
@@ -3,11 +3,14 @@ import React from 'react'
 import AnecdoteForm from './AnecdoteForm';
 import AnecdoteList from './AnecdoteList';
 import Notification from './Notification';
+import AnecdoteFilter from './AnecdoteFilter';
 
 const App = () => (
   <div>
     <Notification />
     <h2>Anecdotes</h2>
+    <AnecdoteFilter />
+    <br/>
     <AnecdoteList />
     <h2>create new</h2>
     <AnecdoteForm />
