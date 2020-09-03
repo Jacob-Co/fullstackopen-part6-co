d783d25d033209eea2efb0ccc5fe1748122171db
Jacobtco <jacobtco@gmail.com>
2020-09-03 11:33:37 +0800

    Created a json server for dev purposes, placed anecdotes inside this server

--- redux-anecdotes/src/components/App.js
+++ redux-anecdotes/src/components/App.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import AnecdoteForm from './AnecdoteForm';
 import AnecdoteList from './AnecdoteList';
 import Notification from './Notification';
 import AnecdoteFilter from './AnecdoteFilter';
 
-const App = () => (
-  <div>
-    <Notification />
-    <h2>Anecdotes</h2>
-    <AnecdoteFilter />
-    <br/>
-    <AnecdoteList />
-    <h2>create new</h2>
-    <AnecdoteForm />
-  </div>
-);
+import anecdoteService from '../services/anecdotes';
+import { initializeAnecdote } from '../reducers/anecdoteReducer';
+
+const App = () => {
+  const disptach = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => disptach(initializeAnecdote(anecdotes)));
+  }, [disptach]);
+
+  return (
+    <div>
+      <Notification />
+      <h2>Anecdotes</h2>
+      <AnecdoteFilter />
+      <br/>
+      <AnecdoteList />
+      <h2>create new</h2>
+      <AnecdoteForm />
+    </div>
+  );
+};
 
 
 export default App;
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -33,9 +24,14 @@ export const newAnecdote = (anecdote) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
+export const initializeAnecdote = (data) => {
+  return {
+    type: 'INIT_ANECDOTE',
+    data
+  };
+};
 
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE':
       return state.map(anecdote => {
@@ -44,6 +40,8 @@ const anecdoteReducer = (state = initialState, action) => {
       });
     case 'NEW_ANECDOTE':
       return [ ...state, action.data ];
+    case 'INIT_ANECDOTE':
+      return action.data;
     default:
       return state;
   }
